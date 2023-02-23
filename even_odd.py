356bb7d1af3d82d8be513ef92e5aacd424c3c85d
Hitendra Narayan Nandan <125806693+Hitu40@users.noreply.github.com>
2023-02-23 19:32:19 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,9 +1,10 @@
-no = int(input("Enter a Number:"))
+series = [1,2,3,4,5,6,7,8,9]
 
 i = 1
 even_cnt = 0
 odd_cnt = 0
-while i <= no:
+
+for i in series:
     if(i % 2 == 0):
         even_cnt += 1
     else:
@@ -13,11 +14,3 @@ while i <= no:
 print("Number of Even Numbers: ",even_cnt)
 print("Number of Odd Numbers: ",odd_cnt)
 
-
-'''
-Output:
-
-Enter a Number:35
-Number of Even Numbers:  17
-Number of Odd Numbers:  18
-'''
