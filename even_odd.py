e0e6cac0dabd3001c3ec95da27fe05329fb59e31
Hitendra Narayan Nandan <125806693+Hitu40@users.noreply.github.com>
2023-02-23 19:34:31 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,6 +1,5 @@
 series = [1,2,3,4,5,6,7,8,9]
 
-i = 1
 even_cnt = 0
 odd_cnt = 0
 
@@ -9,8 +8,9 @@ for i in series:
         even_cnt += 1
     else:
         odd_cnt += 1
-    i += 1
 
 print("Number of Even Numbers: ",even_cnt)
 print("Number of Odd Numbers: ",odd_cnt)
+
+
 
