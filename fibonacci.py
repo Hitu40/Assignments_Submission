f2c9638aaec6ba6573186c6bc8eb13b4ac9912fc
Hitendra Narayan Nandan <125806693+Hitu40@users.noreply.github.com>
2023-02-23 19:39:06 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,14 @@
-no = int(input("Enter a number: ")) # user input
+no = int(input("Enter a number: "))
 x,y = 0,1
 
 while y < no:
     print(y,end=" ")
     x,y = y,x+y
+
+'''
+Output
+
+Enter a number: 50
+1 1 2 3 5 8 13 21 34 
+
+'''
